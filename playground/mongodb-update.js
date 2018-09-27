09734bdd31d2847d0ee6e57de5890e6d8e2287a9
IBM Cloud Garage <ibmcloudgarage@Lavanyas-MacBook-Pro.local>
2018-09-27 16:00:57 -0700

    commit all chnaged files

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -9,14 +9,21 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         return console.log('Unable to cnnect to mongodb server');
     }
     console.log('Connected to Mongo DB Server');
-    //find one and update
+   /*  //find one and update
     db.collection('Todos').findOneAndUpdate({
         _id: new ObjectID('5bac23d12dedc148ba91d376')
-    },{$set:{completed: true}
-
-    }, {returnOriginal: false
+    },{$set:{completed: true}}, {returnOriginal: false
     } ).then((result) => {
         console.log(result);
+    }); */
+    //increment the age by 1
+    db.collection('Users').findOneAndUpdate({_id: new ObjectID('5bac0a871e434e1b849d8277')},
+    {$inc:{age:1}},{returnOriginal: false}).then((result)=> {
+        console.log(result)
+    });
+    db.collection('Users').findOneAndUpdate({_id: new ObjectID('5bac0a871e434e1b849d8277')},
+    {$set:{location:'Irvine'}},{returnOriginal: false}).then((result1)=> {
+        console.log(result1)
     });
      /* db.close();
     console.log('Closing the DB connection..'); */
