47e6295438479f3f0d906564c470f445103ee9af
IBM Cloud Garage <ibmcloudgarage@Lavanyas-MacBook-Pro.local>
2018-10-01 15:47:09 -0700

    Add Hash password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,21 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        // console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$zQt4qdMlazZYh0UkCsSSou49h6Jh9ZuXpvAOZ3BiLPIjE2d7fhxaC';
+
+bcrypt.compare('123', hashedPassword, (err, res) =>{
+    console.log(res);
+});
+/* var data = {
     id: 10
 };
 var token = jwt.sign(data, '123abc');
@@ -9,7 +23,7 @@ console.log(token);
 
 var decoded = jwt.verify(token, '123abc');
 console.log(decoded);
-
+ */
 /* var message = 'I am user number 3';
 var hash = SHA256(message).toString();
 
